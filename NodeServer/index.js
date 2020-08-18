58a31585b5ec131848494fcbcba46e4a8e2586bd
Oreology <hafidhsatyanto@gmail.com>
2020-08-17 22:40:13 -0500

    websocket starting point

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,10 +1,12 @@
+const express = require('express');
+
 // Our HTTP server, serves as a 'hook' for the websocket server
-const port = process.env.PORT || 3000;
-const index = '/index.html';
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
 const server = express()
     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(port, () => console.log('Listening on ${port}'))
+    .listen(PORT, () => console.log('Listening on ${PORT}'))
 
 // Our WebSocket server, takes an HTTP server as an argument
 const { Server } = require('ws');
@@ -12,12 +14,16 @@ const wss = new Server({ server });
 
 wss.on('connection', (ws) => {
     console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
-})
 
-setInterval(() => {
-    wss.clients.forEach((client) => {
-        client.send(new Data().toString());
+    // user sent a message
+    ws.on('message', function(message) {
+        if (message.type === 'utf8') {      // make sure the messages the server receives is a string
+            wss.clients.forEach((client) => {
+                client.sendUTF(message);    // we are simply the messenger
+            });
+        }
     });
-}, 1000);   // every second
 
+    // user disconnected
+    ws.on('close', () => console.log('Client disconnected'));
+});
